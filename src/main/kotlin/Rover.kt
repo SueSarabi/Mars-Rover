c9c52ab9d8452d15d7e2b015eb73d9580695dd5d
Sue Sarabi <sue.mojtahedsarabi@ns.nl>
2025-05-03 13:17:30 +0200

    Switch to JUnit 4 and fix parameterized test setup and use ktlint

--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -13,26 +13,29 @@ class Rover {
     fun go(cms: String) {
         for (c in cms) {
             when (c) {
-                'L' -> when (rs.dd) {
-                    'E' -> rs.dd = 'N'
-                    'N' -> rs.dd = 'W'
-                    'W' -> rs.dd = 'S'
-                    'S' -> rs.dd = 'E'
-                }
+                'L' ->
+                    when (rs.dd) {
+                        'E' -> rs.dd = 'N'
+                        'N' -> rs.dd = 'W'
+                        'W' -> rs.dd = 'S'
+                        'S' -> rs.dd = 'E'
+                    }
 
-                'R' -> when (rs.dd) {
-                    'E' -> rs.dd = 'S'
-                    'S' -> rs.dd = 'W'
-                    'W' -> rs.dd = 'N'
-                    'N' -> rs.dd = 'E'
-                }
+                'R' ->
+                    when (rs.dd) {
+                        'E' -> rs.dd = 'S'
+                        'S' -> rs.dd = 'W'
+                        'W' -> rs.dd = 'N'
+                        'N' -> rs.dd = 'E'
+                    }
 
-                'M' -> when (rs.dd) {
-                    'E' -> rs.xx++
-                    'S' -> rs.yy--
-                    'W' -> rs.xx--
-                    'N' -> rs.yy++
-                }
+                'M' ->
+                    when (rs.dd) {
+                        'E' -> rs.xx++
+                        'S' -> rs.yy--
+                        'W' -> rs.xx--
+                        'N' -> rs.yy++
+                    }
             }
         }
     }
@@ -44,9 +47,7 @@ class Rover {
     val xyd: String
         get() = "${rs.xx} ${rs.yy} ${rs.dd}"
 
-    fun pos(): String {
-        return xyd
-    }
+    fun pos(): String = xyd
 
     constructor() : this("")
 
--- src/test/kotlin/MarsRoverTests.kt
+++ src/test/kotlin/MarsRoverTests.kt
@@ -1,28 +1,40 @@
 import org.example.Rover
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
-class MarsRoverTests {
-    @ParameterizedTest
-    @CsvSource(
-        "1 2 N, ' ', 1 2 N",
-        "1 2 N, L, 1 2 W",
-        "1 2 W, L, 1 2 S",
-        "1 2 S, L, 1 2 E",
-        "1 2 E, L, 1 2 N",
-        "1 2 N, R, 1 2 E",
-        "1 2 E, R, 1 2 S",
-        "1 2 S, R, 1 2 W",
-        "1 2 W, R, 1 2 N",
-        "1 2 N, M, 1 3 N",
-        "1 2 E, M, 2 2 E",
-        "1 2 S, M, 1 1 S",
-        "1 2 W, M, 0 2 W",
-        "1 2 N, LMLMLMLMM, 1 3 N",
-        "3 3 E, MMRMMRMRRM, 5 1 E"
-    )
-    fun `execute commands`(startingPosition: String, instructions: String, expectedOutput: String) {
+@RunWith(Parameterized::class)
+class MarsRoverTests(
+    private val startingPosition: String,
+    private val instructions: String,
+    private val expectedOutput: String,
+) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> =
+            listOf(
+                arrayOf("1 2 N", "", "1 2 N"),
+                arrayOf("1 2 N", "L", "1 2 W"),
+                arrayOf("1 2 W", "L", "1 2 S"),
+                arrayOf("1 2 S", "L", "1 2 E"),
+                arrayOf("1 2 E", "L", "1 2 N"),
+                arrayOf("1 2 N", "R", "1 2 E"),
+                arrayOf("1 2 E", "R", "1 2 S"),
+                arrayOf("1 2 S", "R", "1 2 W"),
+                arrayOf("1 2 W", "R", "1 2 N"),
+                arrayOf("1 2 N", "M", "1 3 N"),
+                arrayOf("1 2 E", "M", "2 2 E"),
+                arrayOf("1 2 S", "M", "1 1 S"),
+                arrayOf("1 2 W", "M", "0 2 W"),
+                arrayOf("1 2 N", "LMLMLMLMM", "1 3 N"),
+                arrayOf("3 3 E", "MMRMMRMRRM", "5 1 E"),
+            )
+    }
+
+    @Test
+    fun `execute commands`() {
         val rover = Rover(startingPosition)
         rover.go(instructions)
         assertEquals(expectedOutput, rover.pos())
